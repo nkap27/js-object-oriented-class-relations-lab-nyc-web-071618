00a8f4a4e76a9593a0617740806d88f0df567e21
github username <github email address>
2018-09-03 16:22:45 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,71 @@
+
+//you can store drivers, and store passengers, and store trips
+let store = {drivers: [], passengers: [], trips:[]}
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name){
+    this.id = ++driverId
+    this.name = name
+    store.drivers.push(this); //push driver instance when initialized
+  }
+
+
+  //driver.trips()
+  trips(){
+    return store.trips.filter(trip =>{
+      return trip.driverId == this.id;
+    })
+  }
+
+  //mapping the passengers from driver.trips(), because trips know about the passenger and driver
+  passengers(){
+    return this.trips().map(trip => {
+      return trip.passenger();
+    })
+  }
+
+}
+
+class Passenger {
+  constructor(name){
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this);
+  }
+
+  trips(){
+    return store.trips.filter(trip => {
+      return trip.passengerId = this.id;
+    });
+  }
+
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
+}
+
+class Trip {
+  constructor(driver, passenger){
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = ++tripId
+    store.trips.push(this);
+  }
+
+  driver(){
+    return store.drivers.find(driver =>{
+      return driver.id === this.driverId;
+    });
+  }
+
+  passenger(){
+    return store.passengers.find(passenger =>{
+      return passenger.id === this.passengerId;
+    });
+  }
+}
